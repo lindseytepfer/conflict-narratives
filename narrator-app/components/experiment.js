6b634b05449b3123cd42cddc6d613ecdbdda5ba2
LT <lindsey.j.tepfer.gr@dartmouth.edu>
2023-12-20 23:42:25 -0500

    reorganized code

--- narrator-app/components/experiment.js
+++ narrator-app/components/experiment.js
@@ -1,9 +1,17 @@
-import React, {useState} from "react";
-import { AudioRecorder } from './audioRecorder.js';
+import React, {useState, useEffect, useRef} from "react";
 
-export const Experiment = ( { pageEvent, socialList, topicList, stream } ) => {
+const mimeType = "audio/webm";
+
+export const Experiment = ( { pageEvent, socialList, topicList } ) => {
     const [index, setIndex] = useState(0)
     const [completed, setCompleted] = useState(0)
+
+    const [permission, setPermission] = useState(false);
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
+    const [audioClip, setAudioClip] = useState([]);
+    const [audio, setAudio] = useState(null);
+    const [stream, setStream] = useState(null);
+    const mediaRecorder = useRef(null);
 
     const advIndex = () => {
         setIndex((prev) => prev +1)
@@ -17,13 +25,109 @@ export const Experiment = ( { pageEvent, socialList, topicList, stream } ) => {
         }
     }
 
+    const getMicPermission = async () => {
+        if ("MediaRecorder" in window) {
+            try {
+                const streamData = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: false,
+                });
+                setPermission(true);
+                setStream(streamData);
+            } catch (err) {
+                alert(err.message)
+            }
+        } else {
+            alert("The MediaRecorder API is not supported in your broswer.")
+            }
+        }
+
+    const startRecording = async () => {
+        setRecordingStatus("recording");
+
+        const media = new MediaRecorder(stream, { type: mimeType });
+        console.log("media variable:", media)
+        mediaRecorder.current = media;
+        mediaRecorder.current.start();
+
+        let localAudioClips = [];
+        mediaRecorder.current.ondataavailable = (event) => {
+           if (typeof event.data === "undefined") return;
+           if (event.data.size === 0) return;
+           localAudioClips.push(event.data);
+        };
+        setAudioClip(localAudioClips);
+      };
+
+      const stopRecording = () => {
+        setRecordingStatus("inactive");
+
+        mediaRecorder.current.stop();
+        mediaRecorder.current.onstop = () => {
+
+           const audioBlob = new Blob(audioClip, { type: mimeType });
+           const audioUrl = URL.createObjectURL(audioBlob);
+           setAudio(audioUrl);
+           setAudioClip([]);
+        };
+      };
+
+      const sendData = () => {
+        // holding this for sending audio files to fireBase
+      }
+    
+
+      const handleAudio = () => {
+          if (audio) {
+            // holding this for adding post-narrative 
+            // questions
+          }
+      }
+
     return(
         <div>
             <p>Tell us about a time you experienced an interpersonal conflict with:
                 a <strong>{socialList[index]}</strong> related to <strong>{topicList[index]}</strong>
             </p>
             
-            <AudioRecorder stream={stream} />
+            {!permission ? (
+                        <button id="enable-audio-button" onClick={getMicPermission} >Enable Audio Permissions</button>
+                ): null
+                }
+
+                {recordingStatus === "inactive" && permission && !audio ?(
+                    <div>
+                    <button id='start-recording-button' onClick={startRecording}>Start recording</button>
+                    </div>
+                    ): null
+                }
+
+                {recordingStatus === "recording" ? ( 
+                    <div>
+                    <button id='stop-recording-button' onClick={stopRecording}>Stop recording</button>
+                    <p>recording in process...</p>
+                    </div>
+                    ): null
+                }
+
+                { audio ? (
+                    <>
+                    <div>
+                    <audio src={audio} controls></audio>
+                    </div>
+                    <div>
+                    <p>Thank you for sharing this narrative. 
+                        Please answer a few additional questions about this scenarioin particular:</p>
+                    <p>How recently have you experienced this conflict?</p>
+                    <p>Have you ever told someone about this conflict?</p>
+                    <p>If you were sharing this story with someone, would you 
+                        want the listener to give you their advice on how to resolve it?</p>
+                    <p>If you were sharing this story with someone, would you 
+                    want the listener to validate your emotions?</p>
+                    </div>
+                    </>
+                    ): null
+                }
 
             <div>
                 <button id="next-button" onClick={switchTopic}> next </button>
--- narrator-app/components/intro.js
+++ narrator-app/components/intro.js
@@ -1,8 +1,7 @@
 import React, {useState} from "react";
 import { v4 as uuid } from 'uuid';
 
-export const Intro = ( { pageEvent, subID, setSubID, stream } ) => {
-    const [permission, setPermission] = useState(false);
+export const Intro = ( { pageEvent, subID, setSubID } ) => {
 
     const setID = () => {
         const x = uuid();
@@ -14,23 +13,6 @@ export const Intro = ( { pageEvent, subID, setSubID, stream } ) => {
           pageEvent();
       }
 
-    const getMicPermission = async () => {
-    if ("MediaRecorder" in window) {
-        try {
-            const streamData = await navigator.mediaDevices.getUserMedia({
-                audio: true,
-                video: false,
-            });
-            setPermission(true);
-            stream.current = streamData;
-        } catch (err) {
-            alert(err.message)
-        }
-    } else {
-        alert("The MediaRecorder API is not supported in your broswer.")
-        }
-    }
-    console.log("Intro.js stream data:", stream)
     return(
         <div className="outer-div">
                 <div className='inner-div'>
@@ -38,30 +20,20 @@ export const Intro = ( { pageEvent, subID, setSubID, stream } ) => {
                     <p> 
                         In this experiment, you will record yourself talking about various categories
                         of interpersonal conflict experiences that you have had. You will be able to select
-                        4 topics to narrate out of 8 potential topics presented to you. After you finish
-                        recording, you will then be asked several questions about that interpersonal
-                        conflict scenario, such as how recently you've experienced, if you have told this
-                        story before, and, hypothetically, if you were to tell someone this story, how you
-                        would want that person to respond.
+                        4 topics to narrate out of 8 potential topics presented to you. 
+                    </p>
+                    <p>  
+                        After you finish recording, you will be asked several questions about that 
+                        interpersonal conflict scenario, such as how recently you've experienced it, if you
+                        have told this story before, and, hypothetically, if you were to tell someone this
+                        story, how you would want that person to respond.
                     </p> 
-
-                    <div id="enable-audio-div">
-                        <p>Before we can proceed, please enable audio + microphone permissions to allow this
-                        experiment to record your narratives:</p>
-
-                        <button id="enable-audio-button" onClick={getMicPermission}>Enable Audio Permissions</button>
-                    </div>
                         
-                    { permission ? ( 
-                        <div>
-                            <p><strong>Permission granted.</strong><br/>
-                            When you are ready to begin, please press the button below:</p>
-                            <button id="start-study-button" onClick={advIntro}>start</button>
-                        </div>
-
-                        ): null
-                    }
-            </div>
+                    <div>
+                        <p>When you are ready to begin, please press the button below:</p>
+                        <button id="start-study-button" onClick={advIntro}>start</button>
+                    </div>
+                </div>
         </div>
     );
 };
