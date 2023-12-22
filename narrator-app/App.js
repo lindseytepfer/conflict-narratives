c7d5b2c2d342456528c6b95004806936de3b77e2
LT <lindsey.j.tepfer.gr@dartmouth.edu>
2023-12-21 23:39:47 -0500

    Minor func changes

--- narrator-app/App.js
+++ narrator-app/App.js
@@ -8,8 +8,7 @@ import { Completion } from "./components/completion";
 const App = () => {
     const [subID, setSubID] = useState(0)
     const [pageState, setPageState] = useState(0);
-    const [socialList, setSocialList] = useState([]);
-    const [topicList, setTopicList] = useState([]);
+    const [topicPairs, setTopicPairs] = useState([]);
 
     const nextPage = () => setPageState((prev) => prev + 1);
     
@@ -18,11 +17,11 @@ const App = () => {
             {(() => {
                 switch (pageState) {
                     case 0:
-                        return <Intro pageEvent={nextPage} subID={subID} setSubID={setSubID} />
+                        return <Intro pageEvent={nextPage} setSubID={setSubID} />
                     case 1:
-                        return <Shuffler pageEvent={nextPage} setTopicList={setTopicList}  setSocialList={setSocialList} />
+                        return <Shuffler pageEvent={nextPage} setTopicPairs={setTopicPairs} />
                     case 2: 
-                        return <Experiment pageEvent={nextPage} topicList={topicList} socialList={socialList} />
+                        return <Experiment pageEvent={nextPage} topicPairs={topicPairs} setTopicPairs={setTopicPairs} />
                     case 3:
                         return <Completion pageEvent={nextPage} />
                     default:
