9abf28cb830f9d23abfb13b1015b65ee322c8c39
LT <lindsey.j.tepfer.gr@dartmouth.edu>
2024-01-22 22:41:45 -0500

    Removed shuffler; sampling function added

--- narrator-app/components/experiment.js
+++ narrator-app/components/experiment.js
@@ -3,19 +3,22 @@ import { Questions } from "./questions.js";
 import { db, storage } from "./firebase.js"
 import { addDoc, collection } from 'firebase/firestore';
 import { ref, uploadBytes } from "firebase/storage";
+var sample = require( '@stdlib/random-sample' );
 
 const mimeType = "audio/wav";
 
-export const Experiment = ( { subID, pageEvent, topicPairs, setTopicPairs } ) => {
-    const [index, setIndex] = useState(0)
+export const Experiment = ( { subID, pageEvent } ) => {
+    // carousel states
+    const [topicPairs, setTopicPairs] = useState([]);
     const [completed, setCompleted] = useState(0)
+
+    // audio states
     const [permission, setPermission] = useState(false);
     const [recordingStatus, setRecordingStatus] = useState("inactive");
     const [audioClip, setAudioClip] = useState([]);
     const [audio, setAudio] = useState(null);
     const [stream, setStream] = useState(null);
     const mediaRecorder = useRef(null);
-
     const [blob, setBlob] = useState(null);
 
     // narrative follow-up question states:
@@ -24,6 +27,74 @@ export const Experiment = ( { subID, pageEvent, topicPairs, setTopicPairs } ) =>
     const [q3, setQ3] = useState(0);
     const [q4, setQ4] = useState(0);
     const [q5, setQ5] = useState(0);
+
+    // generating topics
+    // topic | relationship
+    const categories = [
+        ["something financial","family member"],
+        ["something financial","friend"],
+        ["something financial","romantic partner"],
+        ["something financial","classmate/colleague"],
+        ["something financial","employer or employee"],
+        ["something financial","neighbor or roommate"],
+        ["something financial","business or property owner"],
+        ["something financial","stranger or acquaintance"],
+        ["a social gathering","family member"],
+        ["a social gathering","friend"],
+        ["a social gathering","romantic partner"],
+        ["a social gathering","classmate/colleague"],
+        ["a social gathering","employer or employee"],
+        ["a social gathering","neighbor or roommate"],
+        ["a social gathering","business or property owner"],
+        ["a social gathering","stranger or acquaintance"],
+        ["health","family member"],
+        ["health","friend"],
+        ["health","romantic partner"],
+        ["health","classmate/colleague"],
+        ["health","employer or employee"],
+        ["health","neighbor or roommate"],
+        ["health","business or property owner"],
+        ["health","stranger or acquaintance"],
+        ["identity","family member"],
+        ["identity","friend"],
+        ["identity","romantic partner"],
+        ["identity","classmate/colleague"],
+        ["identity","employer or employee"],
+        ["identity","neighbor or roommate"],
+        ["identity","business or property owner"],
+        ["identity","stranger or acquaintance"],
+        ["parenting","family member"],
+        ["parenting","friend"],
+        ["parenting","romantic partner"],
+        ["parenting","classmate/colleague"],
+        ["parenting","employer or employee"],
+        ["parenting","neighbor or roommate"],
+        ["parenting","business or property owner"],
+        ["parenting","stranger or acquaintance"],
+    ]
+
+    const topicIndices = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+    21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39];
+
+
+    const sampleTopic = () => {
+        const out = sample(topicIndices, {
+            'probs': [0.025244,0.021687,0.027809,0.035271,0.025011,0.023087,0.034805,0.021279,
+                0.021221,0.021746,0.021221,0.033406,0.027109,0.022853,0.030549,0.02262,
+                0.019647,0.021804,0.024311,0.035796,0.028975,0.023145,0.033056,0.021862,
+                0.020813,0.023903,0.023845,0.028334,0.02606,0.022853,0.027168,0.024310,
+                0.019181,0.020275,0.020813,0.028275,0.021513,0.021396,0.027809,0.019938],
+            'size': 1,
+            'replace': true
+        });
+        
+        setTopicPairs(categories[out[0]])
+    }
+
+    useEffect(()=>{
+        sampleTopic();
+    }, [])
+
 
     const dataCollectionRef = collection(db, "responses")
 
@@ -45,8 +116,8 @@ export const Experiment = ( { subID, pageEvent, topicPairs, setTopicPairs } ) =>
         try {
             await addDoc(dataCollectionRef, {
                 userID: subID,
-                relationship:topicPairs[index][0],
-                topic:topicPairs[index][1],
+                relationship:topicPairs[1],
+                topic:topicPairs[0],
                 q1: q1,
                 q2: q2,
                 q3: q3,
@@ -114,7 +185,6 @@ export const Experiment = ( { subID, pageEvent, topicPairs, setTopicPairs } ) =>
 
             setBlob(audioBlob)
         };
-
     };
 
     // Progression across the experiment
@@ -125,32 +195,17 @@ export const Experiment = ( { subID, pageEvent, topicPairs, setTopicPairs } ) =>
         }
     },[completed])
 
-    const advIndex = () => {
-        setIndex((prev) => prev +1)
-    }
-
-    const switchTopic = () => {
-        if (index < 4) { //need to set this to a larger index based on full topic set
-            advIndex();
-        } else {
-            setIndex(0);
-        }
-    }
-
     const handleNext = () => {
         if (audio) {
             setCompleted((prev)=>prev+1)
             sendAudio();
             sendData();
         }
+
         clearRadios();
         setAudio(null);
         setBlob(null);
-        switchTopic();
-    }
-
-    const dropTopic = () =>{
-        return
+        sampleTopic();
     }
 
     console.log("topic pairs:", topicPairs)
@@ -158,7 +213,7 @@ export const Experiment = ( { subID, pageEvent, topicPairs, setTopicPairs } ) =>
     return(
         <div>
             <p>Tell us about a time you experienced an interpersonal conflict with:
-                a <strong>{topicPairs[index][0]}</strong> related to <strong>{topicPairs[index][1]}</strong>
+                a <strong>{topicPairs[1]}</strong> related to <strong>{topicPairs[0]}</strong>
             </p>
             <p><em><font color="#aaacad">to skip this category, please press the 'next' button below</font></em> </p>
             
--- narrator-app/components/intro.js
+++ narrator-app/components/intro.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import { v4 as uuid } from 'uuid';
 
 export const Intro = ( { pageEvent, setSubID } ) => {
